0c0b795b2bf63ba8e652976c67d10048c7e51184
Trayana Koleva <trayana.koleva@gmail.com>
2016-07-06 23:52:04 +0300

    Add files via upload

--- ArraysTasks.java
+++ ArraysTasks.java
@@ -57,12 +57,15 @@ public class ArraysTasks {
         int number;
         int maxCount = 1;
         int maxNumber = 0;
+        int row = 0;
 
         for (int i = 0; i < firstArray.length; i++) {
-            System.out.printf("Enter the %d array element: ", i);
+            System.out.printf("Enter the array element[%d]: ", i);
             firstArray[i] = input.nextInt();
-            System.out.println();
+//            System.out.println();
         }
+
+        System.out.println(java.util.Arrays.toString(firstArray));
 
         for (int i = 0; i < firstArray.length - 1; i++) {
             if (firstArray[i] == firstArray[i + 1]) {
@@ -72,12 +75,19 @@ public class ArraysTasks {
                 if (count >= maxCount) {
                     maxCount = count;
                     maxNumber = number;
+                    row = i;
                 }
             } else {
                 count = 1;
             }
         }
         System.out.println("The count of equal numbers in a row is: " + maxCount + " for number " + maxNumber);
+        System.out.print("{");
+        for (int i = 1; i <= maxCount; i++) {
+            System.out.print(firstArray[row + 1] + " ");
+            row--;
+        }
+        System.out.print("}");
 
         input.close();
     }
@@ -85,22 +95,64 @@ public class ArraysTasks {
     private static void lexArrays() {
         Scanner input = new Scanner(System.in);
 
-        char[] firstArray = {'b', 'A', '%', 'G', '4'};
-        char[] secondArray = {'b', 'a', '#', 'g', '6'};
-        boolean isLexBefore = false;
+        System.out.print("Enter the length of the first array: ");
+        int n = input.nextInt();
+        System.out.print("Enter the length of the second array: ");
+        int k = input.nextInt();
 
-        for (int i = 0; (i < firstArray.length) && (i < secondArray.length); i++) {
-            if(firstArray[i] < secondArray[i]){
-                System.out.println(firstArray[i]);
-                System.out.println(secondArray[i]);
-                isLexBefore = true;
-            } else if (firstArray[i] == secondArray[i]){
-                continue;
-            } else{
-                break;
+        char[] firstArray = new char[n];
+        char[] secondArray = new char[k];
+
+        for (int i = 0; i < firstArray.length; i++) {
+            System.out.printf("Enter %d char element of the first array with %d elements: ", i, n);
+            firstArray[i] = input.next().charAt(0);
+        }
+
+        for (int i = 0; i < secondArray.length; i++) {
+            System.out.printf("Enter %d char element of the second array with %d elements: ", i, k);
+            secondArray[i] = input.next().charAt(0);
+        }
+
+        System.out.println(java.util.Arrays.toString(firstArray));
+        System.out.println(java.util.Arrays.toString(secondArray));
+
+        boolean areEqual = true;
+        if (firstArray.length < secondArray.length){
+            for (int i = 0; i < firstArray.length - 1; i++) {
+                if (firstArray[i] != secondArray[i]){
+                    areEqual = false;
+                    System.out.println("The first array is lexicosomething before the second one.");
+                    break;
+                } else {
+                    System.out.println("The elements are equal, but the first array is before the second one.");
+                }
+            }
+        } else if (firstArray.length > secondArray.length){
+            for (int i = 0; i < secondArray.length - 1; i++) {
+                if (firstArray[i] != secondArray[i]) {
+                    areEqual = false;
+                    System.out.println("The second array is lexicosomething before the first one.");
+                    break;
+                } else {
+                    System.out.println("The elements are equal, but the second array is before the first.");
+                }
+            }
+        } else {
+            for (int i = 0; i < secondArray.length - 1; i++) {
+                if (firstArray[i] != secondArray[i]) {
+                    areEqual = false;
+                    if (firstArray[i] < secondArray[i]){
+                        System.out.println("The first array is lexicosomething before the second array.");
+                    } else if (firstArray[i] > secondArray[i]){
+                        System.out.println("The second array is lexicosomething before the first one.");
+                    } else {
+                        System.out.println("The arrays are equal and are lexicosomething are at one and same place/value.");
+                    }
+                    break;
+                }
             }
         }
-        System.out.println("The first array is before the second array: " + isLexBefore);
+        System.out.println("The arrays are equal: " + areEqual);
 
         input.close();
     }
